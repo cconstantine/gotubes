f3653403550a92a9a753ddc0c0d74ae5c9e1d040
cconstantine <cconstan@gmail.com>
2015-10-18 22:22:06 -0700

    use redis sets to determain where to proxy tcp connections to

--- proxy.go
+++ proxy.go
@@ -4,22 +4,27 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"os"
 	"net"
+	"os"
+	"gopkg.in/redis.v3"
 )
 
 var connid = uint64(0)
-var localAddr = flag.String("l", ":9999", "local address")
+var localAddr = flag.String("l", "localhost:9999", "local address")
 var remoteAddr = flag.String("r", "localhost:80", "remote address")
 var verbose = flag.Bool("v", false, "display server actions")
+var redis_client *redis.Client
 
 func main() {
 	flag.Parse()
-	fmt.Printf("Proxying from %v to %v\n", *localAddr, *remoteAddr)
+	redis_client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	fmt.Printf("Proxying from %v\n", *localAddr)
 
 	laddr, err := net.ResolveTCPAddr("tcp", *localAddr)
-	check(err)
-	raddr, err := net.ResolveTCPAddr("tcp", *remoteAddr)
 	check(err)
 	listener, err := net.ListenTCP("tcp", laddr)
 	check(err)
@@ -32,13 +37,23 @@ func main() {
 		}
 		connid++
 
+		raddr_string, err := redis_client.SRandMember(*localAddr).Result()
+		if err != nil {
+			fmt.Printf("Unable to find a proxy target: %s", err)
+			continue
+		}
+		raddr, err := net.ResolveTCPAddr("tcp", raddr_string)
+		if err != nil {
+			fmt.Printf("Unable to resolve target: %s", err)
+			continue
+		}
 		p := &proxy{
-			lconn:    conn,
-			laddr:    laddr,
-			raddr:    raddr,
-			erred:    false,
-			errsig:   make(chan bool),
-			prefix:   fmt.Sprintf("Connection #%03d ", connid),
+			lconn:  conn,
+			laddr:  laddr,
+			raddr:  raddr,
+			erred:  false,
+			errsig: make(chan bool),
+			prefix: fmt.Sprintf("Connection #%03d ", connid),
 		}
 		go p.start()
 	}
@@ -57,7 +72,7 @@ type proxy struct {
 
 func (p *proxy) log(s string, args ...interface{}) {
 	if *verbose {
-		fmt.Printf(p.prefix+s, args...)
+		fmt.Printf(p.prefix+s+"\n", args...)
 	}
 }
 
@@ -66,7 +81,7 @@ func (p *proxy) err(s string, err error) {
 		return
 	}
 	if err != io.EOF {
-		fmt.Printf(p.prefix+s, err)
+		fmt.Printf(p.prefix+s+"\n", err)
 	}
 	p.errsig <- true
 	p.erred = true
@@ -75,6 +90,7 @@ func (p *proxy) err(s string, err error) {
 func (p *proxy) start() {
 	defer p.lconn.Close()
 	//connect to remote
+
 	rconn, err := net.DialTCP("tcp", nil, p.raddr)
 	if err != nil {
 		p.err("Remote connection failed: %s", err)
@@ -84,11 +100,11 @@ func (p *proxy) start() {
 	defer p.rconn.Close()
 
 	//display both ends
-	p.log("Opened %s >>> %s", p.lconn.RemoteAddr().String(), p.rconn.RemoteAddr().String())
+	p.log("Opened %s >>> %s", p.lconn.LocalAddr().String(), p.rconn.RemoteAddr().String())
 	//bidirectional copy
 	lfinished := make(chan int64)
 	rfinished := make(chan int64)
-	
+
 	go p.pipe(p.lconn, p.rconn, lfinished)
 	go p.pipe(p.rconn, p.lconn, rfinished)
 	//wait for close...
@@ -96,12 +112,11 @@ func (p *proxy) start() {
 	case p.receivedBytes = <-lfinished:
 		p.rconn.Close()
 		p.sentBytes = <-rfinished
-	case p.sentBytes =<-rfinished:
+	case p.sentBytes = <-rfinished:
 		p.lconn.Close()
 		p.receivedBytes = <-lfinished
 	case <-p.errsig:
 	}
-
 
 	p.log("Closed (%d bytes sent, %d bytes recieved)", p.sentBytes, p.receivedBytes)
 }
@@ -111,7 +126,6 @@ func (p *proxy) pipe(src, dst *net.TCPConn, finished chan int64) {
 	finished <- bytesCopied
 }
 
-
 //helper functions
 
 func check(err error) {
@@ -120,6 +134,3 @@ func check(err error) {
 		os.Exit(1)
 	}
 }
-
-
-
